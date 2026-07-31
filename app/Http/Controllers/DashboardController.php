505db4b332ae00722740b086951302b9908d92c2
linna-22 <al2245401@gmail.com>
2026-07-31 22:54:32 +0700

    use one dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,28 +12,37 @@ use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
+// public function index(DashboardService $dashboardService)
+//     {
+//         $user = Auth::user();
+
+//         switch ($user->role) {
+
+//             case 'user':
+//                 return app(EmployeeDashboardController::class)->index();
+
+//             case 'super_admin':
+//                 return view('dashboard.index', [
+//                     'statistics' => $dashboardService->statistics(),
+//                 ]);
+
+//             case 'department_admin':
+//                 abort(501, 'Department dashboard is not implemented yet.');
+
+//             case 'organization_admin':
+//                 abort(501, 'Organization dashboard is not implemented yet.');
+
+//             default:
+//                 abort(403);
+//         }
+//     }
 public function index(DashboardService $dashboardService)
     {
         $user = Auth::user();
 
-        switch ($user->role) {
-
-            case 'user':
-                return app(EmployeeDashboardController::class)->index();
-
-            case 'super_admin':
-                return view('dashboard.index', [
+        return view('dashboard.index', [
                     'statistics' => $dashboardService->statistics(),
+                    'user' => $user,
                 ]);
-
-            case 'department_admin':
-                abort(501, 'Department dashboard is not implemented yet.');
-
-            case 'organization_admin':
-                abort(501, 'Organization dashboard is not implemented yet.');
-
-            default:
-                abort(403);
-        }
     }
 }
