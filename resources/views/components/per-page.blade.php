e2d4789f514021a0c2dfae336619e840a14e5660
linna-22 <al2245401@gmail.com>
2026-08-13 08:44:44 +0700

    add text to filter search page

--- resources/views/components/per-page.blade.php
+++ resources/views/components/per-page.blade.php
@@ -3,14 +3,20 @@
     'value' => 5,
 ])
 
-<select
-    id="{{ $id }}"
-    class="h-10 rounded-2xl border border-gray-200 px-2 bg-white">
+<div class="flex items-center gap-2">
+    <span class="text-sm text-gray-700">
+        បង្ហាញទិន្នន័យ
+    </span>
 
-    <option value="5" @selected($value == 5)>5</option>
-    <option value="10" @selected($value == 10)>10</option>
-    <option value="25" @selected($value == 25)>25</option>
-    <option value="50" @selected($value == 50)>50</option>
-    <option value="100" @selected($value == 100)>100</option>
+    <select
+        id="{{ $id }}"
+        class="h-10 rounded-2xl border border-blue-300 px-2 bg-white">
 
-</select>
+        <option value="5" @selected($value == 5)>5</option>
+        <option value="10" @selected($value == 10)>10</option>
+        <option value="25" @selected($value == 25)>25</option>
+        <option value="50" @selected($value == 50)>50</option>
+        <option value="100" @selected($value == 100)>100</option>
+
+    </select>
+</div>
