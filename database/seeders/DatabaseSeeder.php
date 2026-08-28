442c7d5fc06cb9a5c65d8ed45e627e444c97b6a5
linna-22 <al2245401@gmail.com>
2026-08-28 08:46:22 +0700

    Setup real data on seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,9 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            // OrganizationSeeder::class,
+            AdminSeeder::class,
+            OrganizationStructureSeeder::class,
             UserSeeder::class,
-            GeneralDepartmentofAdministrationandFinanceSeeder::class,
         ]);
     }
 }
